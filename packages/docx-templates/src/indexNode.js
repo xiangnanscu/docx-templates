0b5893f25c518551f1b3ccee4f65414a34ea0c74
xiangnanscu <280145668@qq.com>
2019-06-14 21:55:25 +0800

    global should be indexBrowser

--- packages/docx-templates/src/indexNode.js
+++ packages/docx-templates/src/indexNode.js
@@ -1,13 +1,4 @@
-(function (global, factory) {
-  if (typeof exports === 'object' && typeof module !== 'undefined'){
-    module.exports = factory()
-    module.exports.default = factory()
-  } else if(typeof define === 'function' && define.amd) {
-    define(factory)
-  } else {
-    global = global || self
-    global.createReport = factory();
-  }
-}(this, function () {
-  return require('./mainNode').default;
-}))
+const createReport = require('./mainNode').default;
+
+module.exports = createReport;
+module.exports.default = createReport;
